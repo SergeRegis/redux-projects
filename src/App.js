62ac99d5e8f7a41da0203bf38680853bceb96bf7
bokasa1 <sergendayishimiyee@gmail.com>
2022-09-18 20:41:45 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Counter from './features/counter/Counter';
+
+function App() {
+  return (
+    <main className="App">
+      <Counter />
+    
+    </main>
+  );
+}
+
+export default App;
